9a9c1b44123a17fd1403e204e12816ce0ddddade
Kauanmarcolino <kauanfullstack@gmail.com>
2026-05-25 20:42:30 -0300

    Subindo atvd

---
+++ src/main/java/com/example/demo/controllers/ProdutoController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.ProdutoModel;
+import com.example.demo.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequestMapping(path = "/produtos")
+@RestController
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoModel>> findAllProduto(){
+        return ResponseEntity.ok(produtoService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoModel> buscarProduto(@PathVariable Long id){
+        Optional<ProdutoModel> produto = produtoService.buscarPorId(id);
+
+        if(produto.isPresent()){
+            return  ResponseEntity.ok(produto.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoModel> criarProduto(@RequestBody ProdutoModel produtoModel){
+        ProdutoModel novo = produtoService.criarProduto(produtoModel);
+        return  ResponseEntity.status(201).body(novo);
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/services/ProdutoService.java
@@ -0,0 +1,29 @@
+package com.example.demo.services;
+
+
+import com.example.demo.models.ProdutoModel;
+import com.example.demo.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<ProdutoModel> findAll(){
+        return produtoRepository.findAll();
+    }
+
+    public ProdutoModel criarProduto(ProdutoModel produtoModel){
+        return produtoRepository.save(produtoModel);
+    }
+
+    public Optional<ProdutoModel> buscarPorId(Long id) {
+        return produtoRepository.findById(id);
+    }
+}
